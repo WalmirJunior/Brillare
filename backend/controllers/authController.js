e3bde4614de3f45f2521e20e6f8208d137cf6936
ZDavidXros <61623008+ZDavidXros@users.noreply.github.com>
2025-04-10 17:36:56 -0300

    feat: Validação de Email senha e nome

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const supabase = require('../services/supabaseClient');
+const yup = require('yup');
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -16,40 +17,55 @@ const login = async (req, res) => {
 
 
 const register = async (req, res) => {
-  const { email, password, name } = req.body;
+  const { name, email, password } = req.body;
 
-  if (!email || !password || !name) {
-    return res.status(400).json({ error: 'Preencha todos os campos' });
-  }
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const { data, error } = await supabase
-    .from('users')
-    .insert([
-      {
-        email,
-        password: hashedPassword,
-        name,
-      }
-    ])
-    .select()
-    .single();
+  try {
+    await registerSchema.validate({ name, email, password });
 
-  if (error) {
-    console.error('Erro no registro:', error);
-    return res.status(500).json({ error: 'Erro ao registrar usuário' });
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const { data, error } = await supabase
+      .from('users')
+      .insert([{ name, email, password: hashedPassword }])
+      .select()
+      .single();
+
+    if (error) {
+      return res.status(500).json({ error: 'Erro ao registrar usuário' });
+    }
+
+    const token = jwt.sign({ id: data.id }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+
+    res.status(201).json({
+      message: 'Usuário registrado com sucesso',
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+      },
+      token,
+    });
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
   }
-  const token = jwt.sign({ id: data.id }, process.env.JWT_SECRET, {
-    expiresIn: '1h',
-  });
-  res.status(201).json({
-    message: 'Usuário registrado com sucesso',
-    user: {
-      id: data.id,
-      email: data.email,
-      name: data.name,
-    },
-    token,
-  });
 };
+
+const registerSchema = yup.object().shape({
+  name: yup
+    .string()
+    .min(2, 'Nome deve ter pelo menos 2 letras')
+    .required('Nome é obrigatório'),
+  email: yup
+    .string()
+    .email('Email inválido (use formato: exemplo@dominio.com)')
+    .required('Email é obrigatório'),
+  password: yup
+    .string()
+    .min(6, 'Senha deve ter no mínimo 6 caracteres')
+    .required('Senha é obrigatória'),
+});
+
 
 module.exports = { login, register,  };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const { register } = require('../controllers/authController');
 
 router.post('/login', authController.login);
-router.post('/register', authController.register);
+router.post('/register', register);
 
 module.exports = router;
