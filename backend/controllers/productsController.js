5d07556e3571d3cb7784ff14ef8696d92d3d477a
ZDavidXros <61623008+ZDavidXros@users.noreply.github.com>
2025-04-14 13:03:10 -0300

    feat: Criado a rota put e delete products/id

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -12,4 +12,52 @@ const getAllProducts = async (req, res) => {
   res.status(200).json(data);
 };
 
-module.exports = { getAllProducts };
+const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const updatedData = req.body;
+
+  const { data, error } = await supabase
+    .from('products')
+    .update(updatedData)
+    .eq('id', id)
+    .select(); // retorna o item atualizado
+
+  if (error) {
+    return res.status(500).json({ error: 'Erro ao atualizar o produto' });
+  }
+
+  if (data.length === 0) {
+    return res.status(404).json({ error: 'Produto não encontrado' });
+  }
+
+  res.status(200).json(data[0]);
+};
+
+
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  const { data, error } = await supabase
+    .from('products')
+    .delete()
+    .eq('id', id)
+    .select(); 
+
+  if (error) {
+    return res.status(500).json({ error: 'Erro ao deletar o produto' });
+  }
+
+  if (!data || data.length === 0) {
+    return res.status(404).json({ error: 'Produto não encontrado' });
+  }
+
+  res.status(200).json({ message: 'Produto deletado com sucesso' });
+};
+
+
+
+module.exports = {
+  getAllProducts,
+  updateProduct,
+  deleteProduct,
+};
--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { getAllProducts } = require('../controllers/productsController');
+const {
+    getAllProducts,
+    updateProduct,
+    deleteProduct
+  } = require('../controllers/productsController');
+  
 const authenticateToken = require('../middlewares/authMiddleware');
 
 router.get('/', authenticateToken, getAllProducts);
+router.put('/:id', authenticateToken, updateProduct);
+router.delete('/:id', authenticateToken, deleteProduct);
 
 
 module.exports = router;
